686b7e24e7d4b86d880b55dc28a16e217ad9d4b1
qusaiqeisi <qusaiqeisi6@gmail.com>
2021-04-27 17:41:35 +0300

    lab03

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 **hi iam qusai alqaisi and i am studant in ASAC as softwaer devlopment and this my project to intrduce my self**
 
 ### Author: qusai alqaisi / ASAC studant 
+
+
+** so during my webpage today change you will have more question to mke you know morw about me**
+> i use my javascript from repo of baraah 
+> and we now more regard for loop and array 
--- js/app.js
+++ js/app.js
@@ -1,27 +1,31 @@
 'use strict'
 
-let userName= prompt ('hello. your name please?');
+let userName = prompt('hello. your name please?');
 console.log(userName);
 
-alert(' welcom to my websit ' + userName);
+alert(' welcom to my website ' + userName);
 
+let score = 0;
 
 alert('answer by yes or no please');
 
 //qusation 1
-let userAnswer = prompt('do you know me?');
+let userAnswer = prompt('do you know me is qusai?');
 
-switch(userAnswer.toLowerCase()){
+switch (userAnswer.toLowerCase()) {
     case 'yes':
+    case 'y':
         console.log('perfect');
-      break;
+        score++;
+        break;
     case 'no':
+    case 'n':
         console.log('hi iam qusai');
         break;
-        default:
-            console.log('yes or no answer')
+    default:
+        console.log('yes or no answer')
 }
-if (userAnswer === 'yes'){
+if (userAnswer === 'yes') {
     alert('good to know');
 } else {
     alert('hi iam qusai');
@@ -30,89 +34,140 @@ if (userAnswer === 'yes'){
 //qusation 2
 
 
-let userAnswer2 = prompt('are your age same main ?');
+let userAnswer2 = prompt('did you know that my age is 28 ?');
 
-switch(userAnswer2.toLowerCase()){
+switch (userAnswer2.toLowerCase()) {
     case 'yes':
+    case 'y':
         console.log('perfect');
-        
-      break;
+        score++;
+        break;
     case 'no':
+    case 'n':
         console.log('i am 28');
         break;
-        default:
-            console.log('yes or no answer');
+    default:
+        console.log('yes or no answer');
 }
 
-if (userAnswer2 === 'yes'){
+if (userAnswer2 === 'yes') {
     alert('long life bro');
 } else {
-    alert ('i am 28');
+    alert('i am 28');
 }
 
 //qusation 3
 
 let userAnswer3 = prompt('Are you and me studed togather?');
 
-switch(userAnswer3.toLowerCase()){
+switch (userAnswer3.toLowerCase()) {
     case 'yes':
+    case 'y':
         console.log('perfect');
-        
-      break;
+        score++;
+        break;
     case 'no':
+    case 'n':
         console.log('i studed in MUET');
         break;
-        default:
-            console.log('yes or no answer');
+    default:
+        console.log('yes or no answer');
 }
 
-if (userAnswer3 === 'yes'){
+if (userAnswer3 === 'yes') {
     alert('good to know');
-}else{
-alert('i studed in MUET');
+} else {
+    alert('i studed in MUET');
 }
 
 //qusation 4
 
 let userAnswer4 = prompt('do you have intrest in motivation?');
 
-switch(userAnswer4.toLowerCase()){
+switch (userAnswer4.toLowerCase()) {
     case 'yes':
+    case 'y':
         console.log('self motivation');
-       
-      break;
+        score++;
+        break;
     case 'no':
+    case 'n':
         console.log('we well work on it');
         break;
-        default:
-            console.log('yes or no answer');
+    default:
+        console.log('yes or no answer');
 }
 
-if (userAnswer4 === 'yes'){
+if (userAnswer4 === 'yes') {
     alert('correct answer');
-}else {
-alert('we well work on it');
+} else {
+    alert('we well work on it');
 }
 //qusation 5
 
 let userAnswer5 = prompt('would you like to know more about me ?');
 
-switch(userAnswer5.toLowerCase()){
+switch (userAnswer5.toLowerCase()) {
     case 'yes':
         console.log('perfect');
-      break;
+        score++;
+        break;
     case 'no':
         console.log('thank you for yor time');
         break;
-        default:
-            console.log('yes or no answer');
+    default:
+        console.log('yes or no answer');
 }
 
-if (userAnswer5 === 'yes'){
+if (userAnswer5 === 'yes') {
     alert('my plauser ');
-}else {
+} else {
     alert('thank you for yor time');
 }
+// question 6
+let userAnswer6;
+
+for (let i = 0; i < 4; i++) {
+    userAnswer6 = prompt('guess my length ?')
+    if (userAnswer6 > 176) {
+        alert('too high');
+    }
+    else if (userAnswer6 < 176) {
+        alert('too low');
+    }
+    if (userAnswer6 == 176) {
+        alert('correct');
+        score++;
+        break;
+    }
+}
 
 
-alert(' i well always be happy to know dear ' + userName);
+
+let favDrink = ['tea' , 'greentea' , 'boom' , 'arabiccoffee' ];
+let userAnswer7;
+for (let i = 0; i < 6; i++) {
+    userAnswer7 = prompt('guess what is my favourite drink ?');
+    if (favDrink[0] == userAnswer7) {
+        alert('wow thats correct');
+        score++;
+        break;
+    } else if (favDrink[1] == userAnswer7) {
+        alert('wow thats correct');
+        score++;
+        break;
+    } else if (favDrink[2] == userAnswer7) {
+        alert('wow thats correct');
+        score++;
+        break;
+    }
+    else if (favDrink[3] == userAnswer7) {
+        alert('wow thats correct');
+        score++;
+        break;
+    }
+    alert('try another drink')
+    }
+    console.log(score);
+
+    alert(' i well always be happy to know dear ' + userName + ' my FavBook is ' + favDrink + 'your score' + score );
